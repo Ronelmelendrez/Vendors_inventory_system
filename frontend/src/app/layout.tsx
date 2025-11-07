d671ddea49a5142e6f70bff24c97ef3e71c05eaf
Ronel <ronelmelendrez01@gmail.com>
2025-11-07 12:26:27 +0800

    feat: integrate ThemeProvider into root layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ProductsProvider } from "@/contexts/ProductsContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <ProductsProvider>{children}</ProductsProvider>
-        </AuthProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            <ProductsProvider>{children}</ProductsProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
