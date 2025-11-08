d81ca1084d82366bda89975e8e4bdec7015fe9c8
Ronel <ronelmelendrez01@gmail.com>
2025-11-08 22:08:27 +0800

    feat: add barrel exports for profile components

---
+++ frontend/src/components/profile/index.ts
@@ -0,0 +1,4 @@
+export { UserProfile } from "./UserProfile";
+export { ProfileStats } from "./ProfileStats";
+export { ProfileActivity } from "./ProfileActivity";
+export { ProfileBadges } from "./ProfileBadges";
